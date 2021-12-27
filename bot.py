49da9a617057265c5c763681f8003c9562160279
Heryc Cortés <heryccortes@gmail.com>
2021-12-27 16:45:01 -0600

    Improve /help to show the commands availables

--- bot.py
+++ bot.py
@@ -2,8 +2,13 @@ import telebot as tb
 import pandas as pd
 from auxiliar import bot_token
 
+# leer los csv
 movimientos = pd.read_csv('bases/movimientos.csv', encoding='utf-8')
 cuentas = pd.read_csv('bases/cuentas.csv', encoding='utf-8')
+
+# leer los comandos disponibles del txt
+with open('comandos.txt', encoding='utf8') as f:
+    lines = f.read()
 
 print(bot_token)
 
@@ -17,17 +22,17 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-	bot.reply_to(message, "Comandos disponibles")
+	bot.send_message(message.chat.id, lines)
 
 @bot.message_handler(commands=["movimiento"])
 def add_trans(message):
 	pass
 
-
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.send_message(message.chat.id, 'Probando')
 
-#bot.infinity_polling()
+bot.infinity_polling()
 
+print(lines)
 print('Listo')
