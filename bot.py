a5c779ca8bae7ee142d521ca3ac3e6bbc4097a5d
Heryc Cortés <heryccortes@gmail.com>
2021-12-28 13:06:33 -0600

    Update echo_all

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ transactions = pd.read_csv('db/movimientos.csv', encoding='utf-8')
 accounts = pd.read_csv('db/cuentas.csv', encoding='utf-8')
 
 # read availables commands from the txt file
-with open('comandos.txt', encoding='utf8') as f:
+with open('commands.txt', encoding='utf8') as f:
     lines = f.read()
 
 # auxiliar variables
-cols_mov = ['Tipo', 'Cuenta', 'Cantidad', 'Fecha', 'Descripción']
+cols_trans = ['Tipo', 'Cuenta', 'Cantidad', 'Fecha', 'Descripción']
 
 # auxiliar functions
 def lastn_request(message):
@@ -45,14 +45,14 @@ def lastn_trans(message):
 	except ValueError:
 		bot.send_message(message.chat.id, 'Please especify the number of rows')
 
-	dfi.export(transactions.tail(n_rows), 'images/lastn.png')
+	dfi.export(transactions[cols_trans].tail(n_rows), 'images/lastn.png')
 
 	bot.send_photo(message.chat.id, open('images/lastn.png', 'rb'))
 	
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.send_message(message.chat.id, message.text)
+	bot.send_message(message.chat.id, 'How can I help you?')
 
 bot.infinity_polling()
 
